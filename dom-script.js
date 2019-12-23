d073f6d7b606444e47208f19dd90cdcf8dc98157
learning-dev <ayazm.140895@gmail.com>
2019-12-23 18:27:08 +0530

    add code for 'add items to to-do-list'

---
+++ dom-script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('.add-list');
+console.log(form);
+const taskList = document.getElementById('task-list');
+
+
+function addItemToList(event) {
+  event.preventDefault();
+  const newTask = form.getElementsByTagName('input');
+  const newTaskTag = document.createElement('li');
+  const newTaskTextnode = document.createTextNode(newTask[0].value);
+  newTaskTag.appendChild(newTaskTextnode);
+
+  taskList.appendChild(newTaskTag);
+}
+
+form.addEventListener('submit', addItemToList);
