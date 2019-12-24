4a1315379b42ff8fa93fdb108eaeee84b143602c
learning-dev <ayazm.140895@gmail.com>
2019-12-24 10:41:43 +0530

    add delete functionality to to-do list

--- dom-script.js
+++ dom-script.js
@@ -9,7 +9,28 @@ function addItemToList(event) {
   const newTaskTextnode = document.createTextNode(newTask[0].value);
   newTaskTag.appendChild(newTaskTextnode);
 
+  // delete button
+  const deleteButton = document.createElement('button');
+
+  deleteButton.className = 'delete-btn';
+  deleteButton.appendChild(document.createTextNode('X'));
+
+  newTaskTag.appendChild(deleteButton);
   taskList.appendChild(newTaskTag);
 }
 
+
+function removeItemFromList(event) {
+  if (event.target.classList.contains('delete-btn')) {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm('Are you sure want to delete the task?')) {
+      const task = event.target.parentElement;
+      taskList.removeChild(task);
+    }
+  }
+}
+
+
+// Event Listeners
 form.addEventListener('submit', addItemToList);
+taskList.addEventListener('click', removeItemFromList);
