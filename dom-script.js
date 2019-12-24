c81036e8ca580ecd7296cb672291bf42b79253aa
learning-dev <ayazm.140895@gmail.com>
2019-12-24 13:43:33 +0530

    add complete (strike-off) task feature to  'to-do list'

--- dom-script.js
+++ dom-script.js
@@ -19,6 +19,18 @@ function addItemToList(event) {
   taskList.appendChild(newTaskTag);
 }
 
+function completeTask(event) {
+  if (event.target.localName === 'li') {
+    const strike = document.createElement('s');
+    const text = event.target.textContent.replace('X', '');
+    strike.appendChild(document.createTextNode(text));
+    const li = document.createElement('li');
+    li.appendChild(strike);
+    taskList.removeChild(event.target);
+    taskList.appendChild(li);
+  }
+}
+
 
 function removeItemFromList(event) {
   if (event.target.classList.contains('delete-btn')) {
@@ -26,11 +38,12 @@ function removeItemFromList(event) {
     if (confirm('Are you sure want to delete the task?')) {
       const task = event.target.parentElement;
       taskList.removeChild(task);
+      console.log('inside delete');
     }
   }
 }
 
-
 // Event Listeners
 form.addEventListener('submit', addItemToList);
 taskList.addEventListener('click', removeItemFromList);
+taskList.addEventListener('click', completeTask);
